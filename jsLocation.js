25e01708c102fb2fc2495ddc2592def526807bc2
abroederdorf <broedera@oregonstate.edu>
2016-01-01 19:08:39 -0800

    Add 4.3.15 Reimers Ranch and 4.26.15 Green Mountain Reports

--- jsLocation.js
+++ jsLocation.js
@@ -9,6 +9,7 @@
 document.getElementById("sectionCascadeRiverRoad").style.display = "none";
 document.getElementById('sectionHwy20').style.display = 'none';
 document.getElementById('sectionHwy542').style.display = 'none';
+document.getElementById('sectionNCascades').style.display = 'none';
 document.getElementById('sectionCCascades').style.display = 'none';
 document.getElementById('sectionCWash').style.display = 'none';
 document.getElementById('sectionEnchantments').style.display = 'none';
