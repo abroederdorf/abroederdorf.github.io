9cd393fb455c4f87c9d891be4da6e4dc6e9d2e82
abroederdorf <broedera@oregonstate.edu>
2016-02-07 22:47:01 -0800

    Add Show/Hide All Buttons to Table Pages
    Updated HTML to include show all and hide all buttons, along with
    corresponding javascript files to include functions for the click events

--- jsActivity.js
+++ jsActivity.js
@@ -5,8 +5,35 @@
 * activity page
 *******************************/
 
-//Initialize Activity Page
-document.getElementById('containerActHike').style.display = 'none';
-document.getElementById('containerActClimb').style.display = 'none';
-document.getElementById('containerActCrag').style.display = 'none';
-document.getElementById('containerActSki').style.display = 'none';
+/***************
+* Name: showAllBoxes
+* Description: Sets display attribute to block for all accordion divisions
+* Input: None
+* Output: None, divisions shown
+***************/
+function showAllBoxes(){
+	document.getElementById('containerActHike').style.display = 'block';
+	document.getElementById('containerActClimb').style.display = 'block';
+	document.getElementById('containerActCrag').style.display = 'block';
+	document.getElementById('containerActSki').style.display = 'block';
+}
+
+/***************
+* Name: hideAllBoxes
+* Description: Sets display attribute to none for all accordion divisions
+* Input: None
+* Output: None, divisions hidden
+***************/
+function hideAllBoxes(){
+	document.getElementById('containerActHike').style.display = 'none';
+	document.getElementById('containerActClimb').style.display = 'none';
+	document.getElementById('containerActCrag').style.display = 'none';
+	document.getElementById('containerActSki').style.display = 'none';
+}
+
+//Initialize Date Page
+document.addEventListener('DOMContentLoaded', hideAllBoxes);
+
+//Event Listeners
+document.getElementById("showAll").addEventListener('click', showAllBoxes);
+document.getElementById("hideAll").addEventListener('click', hideAllBoxes);
--- jsDate.js
+++ jsDate.js
@@ -5,7 +5,33 @@
 * date page
 *******************************/
 
+/***************
+* Name: showAllBoxes
+* Description: Sets display attribute to block for all accordion divisions
+* Input: None
+* Output: None, divisions shown
+***************/
+function showAllBoxes(){
+	document.getElementById("containerDate2015").style.display = "block";
+	document.getElementById("containerDate2014").style.display = "block";
+	document.getElementById("containerDate2013").style.display = "block";
+}
+
+/***************
+* Name: hideAllBoxes
+* Description: Sets display attribute to none for all accordion divisions
+* Input: None
+* Output: None, divisions hidden
+***************/
+function hideAllBoxes(){
+	document.getElementById("containerDate2015").style.display = "none";
+	document.getElementById("containerDate2014").style.display = "none";
+	document.getElementById("containerDate2013").style.display = "none";
+}
+
 //Initialize Date Page
-document.getElementById("containerDate2015").style.display = "none";
-document.getElementById("containerDate2014").style.display = "none";
-document.getElementById("containerDate2013").style.display = "none";
+document.addEventListener('DOMContentLoaded', hideAllBoxes);
+
+//Event Listeners
+document.getElementById("showAll").addEventListener('click', showAllBoxes);
+document.getElementById("hideAll").addEventListener('click', hideAllBoxes);
